624f740f2686e7a65593f84a239984248cb49b94
Santiago Ceron <ceronsanti@gmail.com>
2023-08-07 13:04:46 -0300

    Finished first HTTP exercise

--- NodeJS/src/http/index.ts
+++ NodeJS/src/http/index.ts
@@ -3,15 +3,17 @@ import http from "node:http";
 const port = 5000;
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
     const url = req.url?.slice(1);
 
     if (url === "miraclelab") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`<h1>Welcome to MiracleLab</h1><p>This is our website</p>`);
     } else if (url === "about") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`<h1>About us</h1>`);
     } else {
-        res.end(`<h1>Current URL: ${url}</h1>`);
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`<h1>404 Not found</h1>`);
     }
 }).listen(port, () => {
     console.log("Server up in Port:", port);
