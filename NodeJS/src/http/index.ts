9c9cca56b3945432de5bddb2ae539f53d3a5d420
Santiago Ceron <ceronsanti@gmail.com>
2023-08-07 12:33:26 -0300

    Continued HTTP exercise up to item 5

--- NodeJS/src/http/index.ts
+++ NodeJS/src/http/index.ts
@@ -1,10 +1,12 @@
 import http from "node:http";
 
-const port = 3000;
+const port = 5000;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("This is my server");
+
+    console.log("Request's URL:", req.url);    
 }).listen(port, () => {
-    console.log("Server up in Port", port);
+    console.log("Server up in Port:", port);
 });
