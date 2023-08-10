d8c397cd7adcc2b4d6ebeca5d0161d3ef8f20101
Santiago Ceron <ceronsanti@gmail.com>
2023-08-10 10:29:38 -0300

    Finished express exercises

--- NodeJS/src/express/exercise7/index.ts
+++ NodeJS/src/express/exercise7/index.ts
@@ -21,6 +21,10 @@ const shoppingList: Product[] = [
 
 let nextId = 3;
 
+function findItemIndex(id: number): number {
+    return shoppingList.findIndex(item => item.id === id);
+}
+
 app.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end("<h1>Welcome to our shopping list website</h1>");
@@ -35,7 +39,7 @@ app.get('/items/:id', (req, res) => {
     const itemId = Number(req.params.id);
     const item = shoppingList.find(item => item.id === itemId);
 
-    if(!item){
+    if (!item) {
         res.status(404);
         res.json({ error: "Item not found" });
     } else {
@@ -55,6 +59,46 @@ app.post('/items', (req, res) => {
         newItem.id = nextId++;
         shoppingList.push(newItem);
         res.status(201);
+        res.json(shoppingList);
+    }
+})
+
+app.patch('/items/:id', (req, res) => {
+    const itemIndex = findItemIndex(Number(req.params.id));
+    const patchInfo: Product = req.body;
+
+    if (itemIndex === -1) {
+        res.status(404);
+        res.json({ error: "Item not found" });
+    } else {
+        if (!patchInfo.name && !patchInfo.price) {
+            res.status(422);
+            res.json({ error: "One field is required" });
+            return;
+        } else {
+            if (patchInfo.name) {
+                shoppingList[itemIndex].name = patchInfo.name;
+            }
+
+            if (patchInfo.price) {
+                shoppingList[itemIndex].price = patchInfo.price;
+            }
+
+            res.status(200);
+            res.json(shoppingList[itemIndex]);
+        }
+    }
+})
+
+app.delete('/items/:id', (req, res) => {
+    const itemIndex = findItemIndex(Number(req.params.id));
+
+    if (itemIndex === -1) {
+        res.status(404);
+        res.json({ error: "Item not found" });
+    } else {
+        shoppingList.splice(itemIndex, 1);
+        res.status(200);
         res.json(shoppingList);
     }
 })
